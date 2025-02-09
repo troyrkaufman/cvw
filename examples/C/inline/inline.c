c1a1b7dc1fe5f5eb87c0f2afe2b7c1d42a6a2a3e
Troy Kaufman <tkaufman@g.hmc.edu>
2025-02-08 21:42:54 -0800

    Finished problem 3.9 on exercises

--- examples/C/inline/inline.c
+++ examples/C/inline/inline.c
@@ -8,4 +8,14 @@ int main(void) {
     long cycles;
     asm volatile("csrr %0, 0xB00" : "=r"(cycles)); // read mcycle register
     printf ("mcycle = %ld\n", cycles);
+
+    int a = 3;
+    int b = 4;
+    int c;
+
+    // write inline assembly for c = a + 2*b
+    asm volatile("slli %1, %1, 1" : "=r"(c) : "r"(b));
+    asm volatile("add %1, %2, %1" : "=r"(c) : "r"(a), "r"(c));
+
+    printf("c= %d", c); 
 }
