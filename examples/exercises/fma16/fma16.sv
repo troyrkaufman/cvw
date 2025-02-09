c7b03d09d253532d18a86043de18a0692944d6b5
Troy Kaufman <tkaufman@g.hmc.edu>
2025-02-08 19:08:00 -0800

    3 of 4 cases work

--- examples/exercises/fma16/fma16.sv
+++ examples/exercises/fma16/fma16.sv
@@ -3,7 +3,7 @@
 // tkaufman@g.hmc.edu
 // 2/6/25
 /*
-    Top level module that will implement power a optimized half precision FMA functional unit
+    Top level module that will implement a power optimized half precision FMA functional unit
     Acronyms to be aware of:
         - NV = iNValid opeation
         - OF = OverFlow 
@@ -14,31 +14,34 @@
 
 module fma16(input logic  [15:0]    x, y, z, 
              input logic            mul, add, negp, negz, 
-             input logic            roundmode, 
+             input logic  [1:0]          roundmode, 
              output logic [15:0]    result, 
              output logic [3:0]     flags);
 
 logic           sign;
-logic [2:0]     exp;
-logic [21:0]    mantproduct;
-logic [9:0]     shiftfrac;
+logic [4:0]     exp;
+logic [19:0]    multmant;
+logic [9:0]     shiftmant;
 logic [15:0]    finalmant;
 
-// perform multiplication between mantissas
-assign mantproduct = {1'b1, x[9:0]} * {1'b1, y[9:0]};
+assign flags = 4'b0;
 
-// Adjust product to be 10 bits
-assign shiftmant = mantproduct[21:12];
+always_comb begin : fma16
+    multmant = {1'b1, x[9:0]} * {1'b1, y[9:0]};
+    
+    shiftmant = multmant[19:10];
+    
+    sign = x[15] ^ y[15];
+    
+    exp = (x[14:10] + y[14:10]) - 4'd15;
+    
+    result = {sign, exp, shiftmant};
 
-// eXclusive or signed bits
-assign sign = x[15] ^ y[15];
-
-// add exponents
-assign exp = (x[14:10]-4'd15) + (y[14:10]-4'd15);
-
-// finally multiply product of mantissas to sum of exponents
-assign finalmant = shiftmant * (2**exp);
-
-// bit swizzle parts together
-assign result = {sign, exp, finalmant};
+    $display("X: %b", x[14:10]);
+    $display("Y: %b", y[14:10]);
+    $display("Multmant: %b ", multmant);
+    $display("Shiftmant: %b ", shiftmant);
+    $display("exp: %b ", exp);
+    $display("Result: %b ", result);
+end
 endmodule
